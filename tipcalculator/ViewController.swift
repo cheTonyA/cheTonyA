b4bab3b7fea77d9864c1443cca1208b0504f69f9
tony <tonyche@tonys-mbp.myfiosgateway.com>
2021-06-07 00:46:57 -0400

    first commit

---
+++ tipcalculator/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Tony Che on 6/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    
+    @IBOutlet weak var initialBill: UITextField!
+    @IBOutlet weak var tipAmount: UITextField!
+    @IBOutlet weak var total: UITextField!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initialBill.text = "0.00$"
+        total.text = "0.00$"
+        
+        
+    }
+    @IBAction func tipSegment(_ sender: Any) {
+        let tipPercentages = [0.15, 0.18, 0.20]
+                let tipPercentage = tipPercentages[segmentedControl.selectedSegmentIndex]
+                
+                let billAmount = NSString(string: initialBill.text!).doubleValue
+                let tip = billAmount * tipPercentage
+                let totalAmount = billAmount + tip
+                
+                tipAmount.text = "$\(tip)"
+                total.text = "$\(totalAmount)"
+                
+                tipAmount.text = String(format: "$%.2f", tip)
+                total.text = String(format: "$%.2f", totalAmount)
+    }
+}
+
